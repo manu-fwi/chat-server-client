6f536f03e412ce89e435462284d3ed0e3471cc80
manu <eallaud@gmail.com>
2021-09-22 12:34:19 -0400

    Add test client

---
+++ test-client.py
@@ -0,0 +1,37 @@
+# client.py
+import socket
+
+# create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s2.settimeout(0.1)
+
+
+# get local machine name
+host = "192.168.0.100"
+port = int(input("port?"))
+
+# connection to hostname on the port.
+s.connect((host, port))
+m = s.recv(200).decode('utf-8')
+print("received:",m)
+ID = int(m[len("/ID "):])
+s2.connect((host,port+1))
+s2.send(("/connect "+str(ID)).encode('utf-8'))
+print(s2.recv(200).decode('utf-8'))
+while True:
+    msg = input("?")
+    if msg == "STOP":
+        break
+    elif msg!="":
+        s.send(msg.encode('utf-8'))
+        print("command socket:",s.recv(200).decode('utf-8'))
+    try:
+        m = s2.recv(200).decode('utf-8')
+    except:
+        m=""
+    print("messages socket:",m)
+    if m!="":
+        s2.send("/OK".encode('utf-8'))
+s.close()
